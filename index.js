a8669de3ca528d42718a4c6b292b97f5b2a063a0
Arjun Rajaram <85518211+Arjun-R24@users.noreply.github.com>
2023-07-20 16:54:49 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,66 @@
+
+/*
+    Handles clicks on buttons
+*/
+for(let i = 0; i < document.querySelectorAll(".drum").length; i++) {
+    document.querySelectorAll('.drum')[i].addEventListener("click", function() {
+        makeSound(this.innerHTML); 
+        buttonAnimation(this.innerHTML);
+    }); 
+}
+
+/*
+    Handles clicks on key presses
+*/
+document.addEventListener("keydown", function(event) {
+    makeSound(event.key);
+    buttonAnimation(event.key);
+});
+
+function makeSound(key) {
+    var audio;
+    switch (key) {
+        case "w":
+            audio = new Audio("sounds/crash.mp3");
+            audio.play();
+            break;
+        case "a":
+            audio = new Audio("sounds/kick-bass.mp3");
+            audio.play();
+            break;
+        case "s":
+            audio = new Audio("sounds/snare.mp3");
+            audio.play();
+            break;
+        case "d":
+            audio = new Audio("sounds/tom-1.mp3");
+            audio.play();
+            break;
+        case "j":
+            audio = new Audio("sounds/tom-2.mp3");
+            audio.play();
+            break;
+        case "k":
+            audio = new Audio("sounds/tom-3.mp3");
+            audio.play();
+            break;
+        case "l":
+            audio = new Audio("sounds/tom-4.mp3");
+            audio.play();
+            break;
+
+        default:
+            break;
+    }
+}
+
+/*
+    Generates button animation
+*/
+function buttonAnimation(currentKey) {
+    document.querySelector("." + currentKey).classList.add("pressed");
+
+    setTimeout(function() {
+        document.querySelector("." + currentKey).classList.remove("pressed");
+      }, 100);
+}
